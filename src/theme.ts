75fc7377611a11809b9199b4a3a38945dc4c4b08
jameschain <james.cordova423@gmail.com>
2021-10-13 17:28:00 -0400

    chore: use mui default dark theme

--- src/theme.ts
+++ src/theme.ts
@@ -3,13 +3,7 @@ import { createTheme } from '@mui/material/styles';
 // A custom theme for this app
 const theme = createTheme({
   palette: {
-    background: {
-      default: '#222222',
-      paper: '#0D2232',
-    },
-    text: {
-      primary: '#ffffff',
-    },
+    mode: 'dark',
   },
 });
 
